d6d39c476a02da1ad0da801f48203dc750718ac7
lucent1090 <lucent1090@gmail.com>
2019-01-26 17:09:22 +0800

    style: run prettier on test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import 'jest-dom/extend-expect'
+import 'jest-dom/extend-expect';
 import App from '../App';
 
 test('it should render weather component and input line for todo list', () => {
--- src/__tests__/FancyInput.test.js
+++ src/__tests__/FancyInput.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render, cleanup } from 'react-testing-library';
-import 'jest-dom/extend-expect'
+import 'jest-dom/extend-expect';
 import FancyInput from '../FancyInput';
 
-afterEach(cleanup)
+afterEach(cleanup);
 
 test('it should render a add button', () => {
   const { getByText } = render(<FancyInput />);
   expect(getByText('Add')).toBeInTheDocument();
 });
-
-// test('it should')
